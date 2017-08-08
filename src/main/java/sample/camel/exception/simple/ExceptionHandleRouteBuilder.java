5a8b75efbf763ac5fb7f4e8a20600e5baeab65f4
yamashiro.ryota <yamashiro0110@gmail.com>
2017-08-09 00:23:42 +0900

    add exception handle processor

--- src/main/java/sample/camel/exception/simple/ExceptionHandleRouteBuilder.java
+++ src/main/java/sample/camel/exception/simple/ExceptionHandleRouteBuilder.java
@@ -1,17 +1,25 @@
 package sample.camel.exception.simple;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 /**
  * Created by yamashiro-r on 2016/09/12.
+ *
+ * @see <a href="http://camel.apache.org/try-catch-finally.html">try-catch-finally</a>
+ * @see <a href="http://camel.apache.org/exception-clause.html">exception-clause</a>
  */
 @SpringBootApplication
 @Component
 public class ExceptionHandleRouteBuilder extends RouteBuilder {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandleRouteBuilder.class);
 
     public static void main(final String[] args) {
         SpringApplication.run(ExceptionHandleRouteBuilder.class, args);
@@ -20,6 +28,13 @@ public class ExceptionHandleRouteBuilder extends RouteBuilder {
     private Processor throwExceptionProcessor = exchange -> {
         exchange.getIn().setBody("例外を発生させます");
         throw new IllegalStateException("例外を発生させます");
+    };
+
+    private Processor exceptionHandleProcessor = exchange -> {
+        Throwable throwable = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Throwable.class);
+        Assert.notNull(throwable);
+        LOGGER.error("エラーが発生しました", throwable);
+        // TODO: exception handling
     };
 
     @Override
@@ -32,6 +47,7 @@ public class ExceptionHandleRouteBuilder extends RouteBuilder {
                 .to("log:例外発生後")
                 .doCatch(Exception.class)
                 .to("log:例外をcatchしました")
+                .process(this.exceptionHandleProcessor)
                 .doFinally()
                 .to("log:例外処理の終了")
                 .end();
